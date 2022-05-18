e48abee787ffc0bd15f99c90e8f107c03a2eb324
DarioUrbina <urbiname@usc.com>
2022-05-17 17:55:18 -0700

    Testig some functions

---
+++ testing_code.py
@@ -0,0 +1,9 @@
+
+from all_functions import *
+
+
+[babbling_kinematics, babbling_activations] = babbling_fcn(simulation_minutes=20)
+
+#to visualize simulation change Mj_render to True in all_functions.py
+
+
